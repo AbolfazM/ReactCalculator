bcba7aa9e38ca604a24e626e9fa0e05578e51eef
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-04 15:36:49 +0330

    Done Styles!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-import './App.css';
+import './index.css';
 
 const App = () => {
-  return(
-    <div>
-        App
-    </div>
-  );
+
+	const createDigits = () => {
+
+		const digits = [];
+
+		for (let i = 1; i < 10; i++) {
+			digits.push(
+				<button key={i}> {i} </button>
+			);
+		}
+		return digits;
+	}
+	return(
+		<div className="App">
+			<div className="calculator">
+
+				<div className="display">
+					<span>(0)</span> 0
+				</div>
+
+				<div className="operators">
+					<button>/</button>
+					<button>*</button>
+					<button>+</button>
+					<button>-</button>
+
+					<button>DEL</button>
+				</div>
+
+				<div className="digits">
+
+					{ createDigits() }
+					<button>0</button>
+					<button>.</button>
+					<button>=</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
