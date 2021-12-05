7089d4f97f1701335f4f112bbc5f20005bc12fac
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-05 14:27:01 +0330

    complete close_open styles

--- src/Components/userPanel.jsx
+++ src/Components/userPanel.jsx
@@ -6,6 +6,7 @@ const UserPanel = () => {
     return(
         <div className="Panel">
             <div>
+                <span className="CloseOpen">X</span>
                 <img className="Userprofile" src={userProfile} alt={`user profile`} />
             </div>
         </div>
