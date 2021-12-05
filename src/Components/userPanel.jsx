0bfab2c95bf5258c0e5bc88efdbfb8dec7890117
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-05 14:01:50 +0330

    add comments!

--- src/Components/userPanel.jsx
+++ src/Components/userPanel.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import userProfile from './userprofile.png';
 import './styles.css';
 
 const UserPanel = () => {
     return(
-        <div className="Panel"></div>
+        <div className="Panel">
+            <div>
+                <img src={userProfile} alt={`user profile`} />
+            </div>
+        </div>
     );
 }
 
